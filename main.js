39cee5a11ce6748c0164d038d32e2425475d43dc
Raphael Lima <raphaellima365@gmail.com>
2024-03-28 11:09:20 -0300

    alterando o código-2

--- main.js
+++ main.js
@@ -13,11 +13,9 @@ function Jetski(modelo, fabricante, cilindrada, anoMod, anoFab, velMax, tanque) 
     Moto.call(this, modelo, fabricante, cilindrada, anoMod, anoFab)
 }
 
-function Honda(modelo, fabricante, cilindrada, anoMod, anoFab, velMax, tanque, consumo, reserva) {
+function Honda(modelo, fabricante, cilindrada, anoMod, anoFab, consumo, reserva) {
     this.consumo = consumo
     this.reserva = reserva
-
-    Jetski.call(this, modelo, fabricante, cilindrada, anoMod, anoFab, velMax, tanque);
 
     Moto.call(this, modelo, fabricante, cilindrada, anoMod, anoFab)
 
@@ -27,7 +25,7 @@ function Honda(modelo, fabricante, cilindrada, anoMod, anoFab, velMax, tanque, c
 
 const yamaha = new Moto("Ybr", "Yamaha", 125, 2010, 2009);
 const yamahaJet = new Jetski("FxCruiser", "Yamaha", 1052, 2015, 2014, 110, 57);
-const carro = new Honda("civic", "honda", 2000, 2012, 2011, 180, 40, "15,9km/l", 15);
+const carro = new Honda("civic", "honda", 2000, 2012, 2011, "15,9km/l", 15);
 
 console.log(yamaha)
 console.log(yamahaJet)
