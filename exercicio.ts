2dec0c54a8c7be53907e1814dad254d90b410ba9
Raphael Lima <raphaellima365@gmail.com>
2024-04-09 01:39:12 -0300

    initial commit

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function calcArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+function dizOla(nome: string): string {
+    return "Olá! " + nome;
+}
+
+console.log(calcArea(30, 50))
+console.log(dizOla('Raphael'))
